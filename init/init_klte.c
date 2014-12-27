87b28312e735773d37ba9c2fe3933bd76c635bd5
scoty755 <scoty755@gmail.com>
2014-12-27 13:31:16 +0900

    init: refine setprop

--- init/init_klte.c
+++ init/init_klte.c
@@ -56,7 +56,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
         property_set("ro.build.fingerprint", "samsung/SCL23/SCL23:4.4.2/KOT49H/SCL23KDU1AND1:user/release-keys");
         property_set("ro.build.description", "kltekdi-user 4.4.2 KOT49H SCL23KDU1AND1 release-keys");
-        property_set("ro.product.model", "SM-G900J");
+        property_set("ro.product.model", "SCL23");
         property_set("ro.product.device", "kltekdi");
         cdma_properties("1", "44054", "kltekdi");
     /* TODO: Add KDDI MVNOs */
@@ -73,7 +73,7 @@ void cdma_properties(char default_cdma_sub[], char operator_numeric[],
     property_set("ro.cdma.home.operator.numeric", operator_numeric);
     property_set("ro.cdma.home.operator.alpha", operator_alpha);
     property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
-    property_set("ro.telephony.default_network", "10");
+    property_set("ro.telephony.default_network", "5");
     property_set("ro.telephony.ril.v3", "newDriverCallU,newDialCode");
     property_set("telephony.lteOnCdmaDevice", "1");
 }
