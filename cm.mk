0e6534423420a946380348bd59a7b8590dd5c295
scoty755 <scoty755@gmail.com>
2015-01-31 12:03:57 +0900

    kltekdi: Re-order product inheritance

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, device/samsung/kltekdi/full_kltekdi.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/kltekdi/full_kltekdi.mk)
 
 PRODUCT_DEVICE := kltekdi
 PRODUCT_NAME := cm_kltekdi
