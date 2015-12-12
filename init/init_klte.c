dccd3cc9332acb4270b84267b0f00f3abe13daeb
scoty755 <scoty755@gmail.com>
2015-12-12 09:49:59 +0900

    kltekdi: init: Fix property
    Change-Id: I2075c2c2090e1d07e9651508e63e88bd00cf4033

--- init/init_klte.c
+++ init/init_klte.c
@@ -58,8 +58,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "kltekdi-user 4.4.2 KOT49H SCL23KDU1AND1 release-keys");
         property_set("ro.product.model", "SCL23");
         property_set("ro.product.device", "kltekdi");
-        cdma_properties("1", "44054", "kltekdi");
-    /* TODO: Add KDDI MVNOs */
+        property_set("telephony.sms.pseudo_multipart", "1");
+        cdma_properties("1", "44054", "KDDI");
 
     property_get("ro.product.device", device);
     strlcpy(devicename, device, sizeof(devicename));
